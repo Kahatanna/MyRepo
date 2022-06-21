7c817c2b1eb91672cc4a890c5cde7661c0b7432c
Elena <kahatanna@gmail.com>
2022-06-21 22:51:00 +0300

    Example 005 IF-Else

---
+++ Example_005_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Представьтесь программе: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("УРА! Это же МАША!");
+}
+else
+{
+    Console.WriteLine("Привет, " + username + "!");
+}
